998a6b06e2e3ed12dd5caaabc674bde234937705
Jhamat Mahbubani <jmahbubani@gmail.com>
2021-08-05 12:50:24 +0100

    first draft of creating order csv directly from config

---
+++ R/create_order_csv.R
@@ -0,0 +1,39 @@
+library(jsonlite)
+
+args <- commandArgs(trailingOnly = TRUE)
+config_json_path <- args[1]
+species_map_dir <- args[2]
+
+config <- fromJSON(config_json_path)
+
+order_df <- data.frame()
+for (species in names(config$SPECIES_LIST)) {
+    species_prj <- config$SPECIES_LIST[[species]]$ENA_PRJ
+    print(species_prj)
+    species_units <- config$SPECIES_LIST[[species]]$RUN_ACCESSION
+    print(species_units)
+    species_url <- paste0("https://www.ebi.ac.uk/ena/portal/api/filereport?accession=",species_prj,"&result=read_run&fields=run_accession,library_name,nominal_length,fastq_ftp,study_accession,scientific_name,instrument_platform,fastq_bytes,fastq_md5,sample_accession&format=tsv&download=true")
+    print(species_url)
+    species_table <- read.table(file = species_url, sep = '\t', header = TRUE)
+    species_table$species <- species
+    species_table <- species_table[species_table$run_accession %in% species_units, ]
+    
+    max_in_gb <- max(as.numeric(unlist(strsplit(as.character(species_table[, "fastq_bytes"]), ";")))) / 1024 / 1024 / 1024
+    n_mapping_pieces <- round((max_in_gb / 25) * 200)
+    if ((n_mapping_pieces < 10) | (1000 < n_mapping_pieces)) {
+        stop("bad assumption!")
+    }
+    species_table$n_mapping_pieces <- n_mapping_pieces
+
+    species_table[is.na(species_table$nominal_length), 'nominal_length'] <- 200
+
+    order_df <- rbind(order_df, species_table)
+}
+
+order_df$flowcell_barcode <- "X1"
+order_df$flowcell_lane <- "1"
+order_df$mapping_queue <- "short.qc@@short.hge"
+
+order_csv_path <- paste0(species_map_dir,"/",config$SPECIES_ORDER,".csv")
+write.csv(order_df, order_csv_path, row.names = FALSE)
+message("Order dataframe saved in ", order_csv_path)
--- run.sh
+++ run.sh
@@ -31,51 +31,14 @@ fi
 
 echo "Running with ${jobs} cores"
 
-# put this all in config
-## if [ "${species}" == "human" ] || [ "${species}" == "chimp" ] || [ "${species}" == "gorilla" ] || [ "${species}" == "orangutan" ] || [ "${species}" == "baboon" ] || [ "${species}" == "marmoset" ] ||  [ "${species}" == "baboon" ] || [ "${species}" == "macaque" ] || [ "${species}" == "snubnosedmonkey" ] || [ "${species}" == "vervet" ] || [ "${species}" == "squirrelmonkey" ] || [ "${species}" == "neanderthal" ]  || [ "${species}" == "denisovan" ]
-
-# if [ "${species}" == "human" ] || [ "${species}" == "chimp" ] || [ "${species}" == "gorilla" ] || [ "${species}" == "orangutan" ] || [ "${species}" == "neanderthal" ]  || [ "${species}" == "denisovan" ]
-# then
-#     SPECIES_ORDER="hominoidea"
-# elif [ "${species}" == "baboon" ] || [ "${species}" == "marmoset" ] ||  [ "${species}" == "baboon" ] || [ "${species}" == "macaque" ]
-# then
-#     SPECIES_ORDER="cercopithecoidea"
-# elif [ "${species}" == "FAM" ] || [ "${species}" == "caroli" ] || [ "${species}" == "CAST_EiJ" ] || [ "${species}" == "WSB_EiJ" ] || [ "${species}" == "SPRET_EiJ" ] || [ "${species}" == "PWK_PhJ" ]
-# then
-#     SPECIES_ORDER="mice"
-# elif [ "${species}" == "felidae" ]
-# then
-#     SPECIES_ORDER="felidae"
-# elif [ "${species}" == "zebra_finch" ] ||  [ "${species}" == "rifleman" ] ||  [ "${species}" == "medium_ground_finch" ] ||  [ "${species}" == "long_tailed_finch" ] || [ "${species}" == "kea" ] || [ "${species}" == "golden_collared_manakin" ] ||  [ "${species}" == "double_barrelled_finch" ] || [ "${species}" == "american_crow" ]
-# then
-#     SPECIES_ORDER="avian"
-# elif [ "${species}" == "arctic_char" ] ||  [ "${species}" == "atlantic_salmon" ] ||  [ "${species}" == "chinook" ] ||  [ "${species}" == "coho" ] || [ "${species}" == "rainbow_trout" ]
-# then
-#     SPECIES_ORDER="salmon"
-# elif [ "${species}" == "big_brown_bat" ] ||  [ "${species}" == "brandts_bat" ] ||  [ "${species}" == "davids_bat" ] ||  [ "${species}" == "red_bat" ]
-# then
-#     SPECIES_ORDER="bat"
-# elif [ "${species}" == "lizards" ] || [ "${species}" == "A_angusticeps" ] || [ "${species}" == "A_chlorocyanus" ] || [ "${species}" == "A_cristatellus" ] || [ "${species}" == "A_cybotes" ] || [ "${species}" == "A_evermanni" ] || [ "${species}" == "A_grahami" ] || [ "${species}" == "A_insolitus" ] || [ "${species}" == "A_lineatopus" ] || [ "${species}" == "A_occultus" ] || [ "${species}" == "A_sagrei" ] || [ "${species}" == "A_valencienni" ]
-# then
-#     SPECIES_ORDER="lizards"
-# elif [ "${species}" == "canidae" ] || [ "${species}" == "african_golden_wolf" ] || [ "${species}" == "african_hunting_dog" ] || [ "${species}" == "andean_fox" ] || [ "${species}" == "coyote" ] || [ "${species}" == "dhole" ] || [ "${species}" == "dog" ] || [ "${species}" == "golden_jackal" ] || [ "${species}" == "grey_fox" ] || [ "${species}" == "island_fox" ] || [ "${species}" == "red_fox" ]
-# then
-#     SPECIES_ORDER="canidae"
-# elif [ "${species}" == "ursidae" ]
-# then
-#     SPECIES_ORDER="ursidae"
-# else
-#     echo Cannot determine order
-#     exit 1
-# fi
-
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 export ORDER_CONFIG="${SCRIPTPATH}/${order_config}"
 
 # From jq docs on how to load variables
-eval "$(jq -r '@sh "SPECIES_LIST=( \([.SPECIES_LIST[]]) ) SPECIES_ORDER=\(.SPECIES_ORDER)"' ${order_config})"
+# SPECIES_LIST=( $(jq -r '.SPECIES_LIST | keys[]' ${order_config}) )
+eval "$(jq -r '@sh "SPECIES_ORDER=\(.SPECIES_ORDER)"' ${order_config})"
 eval "$(jq -r '@sh "SPECIES_MAP_DIR_NAME=\(.SPECIES_MAP_DIR_NAME)"' config/filenames.json)"
 
 echo "Motif Death output in ${ANALYSIS_DIR}"
@@ -87,13 +50,7 @@ then
     rm $ORDER_CSV
 fi
 
-echo "species,units,1,2,n_mapping_pieces,mapping_queue,lb,lb_insert_size,flowcell_barcode,flowcell_lane" > $ORDER_CSV
-
-for name in ${SPECIES_LIST[@]}; do
-    jq -r '.units = (.units | to_entries[]) |
-        [.species, .units.key, .units.value."1", .units.value."2", .n_mapping_pieces, .mapping_queue, .units.value.lb, .units.value.lb_insert_size, .units.value.flowcell_barcode, .units.value.flowcell_lane] | @csv' \
-        ${SPECIES_MAP_DIR_NAME}/${name}.json >> $ORDER_CSV
-done
+R -f R/create_order_csv.R --args $ORDER_CONFIG $SPECIES_MAP_DIR_NAME
 
 LOG_DIR=${ANALYSIS_DIR}logs/
 mkdir -p ${ANALYSIS_DIR}
