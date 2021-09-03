e9e80838f4c6fd0ec088a7fc269b52a2a2dd41fe
Jhamat Mahbubani <jmahbubani@gmail.com>
2021-09-03 11:13:18 +0100

    place obsolete files in different folder

---
+++ obsolete/prepare_inputs.R
@@ -0,0 +1,207 @@
+## setwd("/Users/robert davies/Dropbox/Hotspot Death/Other ENA excel")
+
+##
+## workflow to add a species
+## 1) identify one or more papers / ENA projects with data of interest
+## 2) download relevant .tsv or .txt files from ENA with information about the raw data (e.g. for project ID PRJNA313910, go to https://www.ebi.ac.uk/ena/browser/view/PRJNA313910, select all columns, and manually download tsv file with information
+## 3) manually make "get_<species>_info" with manually curated information from the above
+##   a) make a list with one character vector entry per species in some arbitrary order
+##   b) for each species the character vector has length 5 with the following elements in the following order
+##      - name of the sample in the ENA file (sample_alias column)
+##      - scientific name
+##      - working name for the analysis (lower case, simple form)
+##      - file to read in
+##      - number of mapping pieces
+##   c) manually edit "get_b" function that loads ENA file and sub-selects specific entries from ENA file, e.g. possibly taking a subset for space reasons, and only taking Illumina short read ideally high quality HiSeq PE 2x150 or similar (rather than mate pair, etc)
+## 3 - appendix - a URL like this might work to download PRJ file https://www.ebi.ac.uk/ena/portal/api/filereport?accession=PRJNA317745&result=read_run&fields=study_accession,secondary_study_accession,sample_accession,secondary_sample_accession,experiment_accession,run_accession,submission_accession,tax_id,scientific_name,instrument_platform,instrument_model,library_name,nominal_length,library_layout,library_strategy,library_source,library_selection,read_count,base_count,center_name,first_public,last_updated,experiment_title,study_title,study_alias,experiment_alias,run_alias,fastq_bytes,fastq_md5,fastq_ftp,fastq_aspera,fastq_galaxy,submitted_bytes,submitted_md5,submitted_ftp,submitted_aspera,submitted_galaxy,submitted_format,sra_bytes,sra_md5,sra_ftp,sra_aspera,sra_galaxy,cram_index_ftp,cram_index_aspera,cram_index_galaxy,sample_alias,broker_name,sample_title,nominal_sdev,first_created&format=tsv&download=true
+
+R_DIR <- Sys.getenv("R_DIR")
+ENA_DIR <- Sys.getenv("ENA_DIR")
+group <- commandArgs(trailingOnly = TRUE)
+
+# TODO Robbie temp
+if (1 == 0) {
+
+    R_DIR <- "~/proj/motif_death/R/"
+    ENA_DIR <- "/well/davies/shared/motif_death_resources/Other ENA excel/"
+    ANALYSIS_DIR='/well/myers/rwdavies/primates/'
+    group <- "artiodactyla"
+    
+}
+
+## hacky - move to motif_death home
+setwd(file.path(dirname(R_DIR)))
+source(file.path(R_DIR, "prepare_inputs_functions.R"))
+
+## ugh, good enough for now
+library(jsonlite)
+config_json_path <- file.path(dirname(R_DIR), "config", "filenames.json")
+config <- fromJSON(config_json_path)
+SPECIES_MAP_DIR_NAME <- config[["SPECIES_MAP_DIR_NAME"]]
+print(SPECIES_MAP_DIR_NAME)
+
+
+## choose
+if (group == "artiodactyla") {
+    info <- get_artiodactyla_info()
+} else if (group == "avian") {
+    info <- get_bird_info()
+} else {
+    stop("fix this file!")
+}
+## info <- get_canid_info()
+## info <- get_bird_info()
+## info <- get_salmon_info()
+## info <- get_bat_info()
+## info <- get_bear_info()
+
+
+i_species <- 1
+
+
+lapply(1:length(info), function(i_species) {
+    ##
+    print(i_species)
+    sample_alias <- info[[i_species]][1]
+    sample_name <- info[[i_species]][3]
+    print(sample_name)
+    paper <- info[[i_species]][4]
+    n_mapping_pieces <- as.integer(info[[i_species]][5])
+    ##
+    b <- get_b(paper, sample_alias)
+    b[, c(grep("run_accession", colnames(b)), grep("fastq", colnames(b)))] 
+    ##
+    print(sample_name)
+    print(b[, c("instrument_model", "sample_accession", "nominal_length", "nominal_sdev", "read_length", "sample_alias", "scientific_name", "fastq_bytes")])
+    ##    
+    units <- lapply(1:nrow(b), function(i) {
+        if (
+        ("fastq_md5" %in% colnames(b)) &
+        ("fastq_ftp" %in% colnames(b))        
+        ) {
+            a1 <- basename(strsplit(as.character(b[i, "fastq_ftp"]), ";")[[1]])
+            a2 <- strsplit(as.character(b[i, "fastq_md5"]), ";")[[1]]
+            md5_1 <- a2[grep("_1.fastq", a1)]
+            md5_2 <- a2[grep("_2.fastq", a1)]            
+        } else {
+            md5_1 <- NA
+            md5_2 <- NA            
+        }
+        x <- strsplit(as.character(b[i, "fastq_ftp"]), ";")[[1]]
+        x2 <- sapply(x, basename)
+        nominal_length <- b[i, "nominal_length"]
+        if (is.na(nominal_length)) {
+            nominal_length <- 200
+        }
+        lb <- b[i, "library_name"]
+        if (is.na(lb) | lb == "") {
+            lb <- paste0("lb", i)
+        }
+        o <- list(
+            "1" = paste0("ftp://", x[grep("_1.fastq.gz", x2)]),
+            "2" = paste0("ftp://", x[grep("_2.fastq.gz", x2)]),
+            "md5_1" = md5_1,
+            "md5_2" = md5_2,            
+            "lb" = lb,
+            "lb_insert_size" = as.character(nominal_length),
+            "flowcell_barcode" = paste0("X", i),
+            "flowcell_lane" = as.character(i)
+        )
+    })
+    names(units) <- as.character(b[, "run_accession"])
+    ##
+    if (("fastq_bytes" %in% colnames(b)) & (FALSE == (sample_name %in% c("coho", "chinook")))) {
+        max_in_gb <- max(as.numeric(unlist(strsplit(as.character(b[, "fastq_bytes"]), ";")))) / 1024 / 1024 / 1024
+        n_mapping_pieces <- round((max_in_gb / 25) * 200)
+        if ((n_mapping_pieces < 10) | (1000 < n_mapping_pieces)) {
+            stop("bad assumption!")
+        }
+    }
+    ##
+    library("jsonlite")
+    o <- list(
+        "species" = sample_name,
+        "n_mapping_pieces" = n_mapping_pieces,
+        "platform" = "Illumina",
+        "mapping_queue" = "short.qc",
+        "units" = units
+    )
+    ##
+    json_to_out <- toJSON(o, pretty = TRUE, auto_unbox = TRUE)
+    print(json_to_out)
+    cat(json_to_out, file = file.path(SPECIES_MAP_DIR_NAME, paste0(sample_name, ".json")))
+    ##
+    gb <- sum(as.numeric(unlist(strsplit(as.character(b[, "fastq_bytes"]), ";")))) / 1024 / 1024 / 1024
+    return(c(gb, sample_name))
+})
+
+
+
+
+quit()
+
+
+
+## s <- data.table::fread("~/Downloads/speclist.txt", skip = 60, sep = "&", data.table = FALSE)
+## ## re-format
+## c <- c(grep(":", s[, 1]), nrow(s) + 1)
+## out <- array("", c(length(c), 3))
+## colnames(out) <- c("N", "S", "C")
+
+## for(i_c in 1:length(c)) {
+##     w <- (c[i_c]:(c[i_c + 1] - 1))
+##     local <- s[w, ]
+##     local[1] <- substr(local[1], 18, 1000)
+##     ##a <- sapply(strsplit(s[w, ], ":"), function(x) x[length(x)])
+##     b <- sapply(strsplit(local, "="), I)
+##     b[1, ] <- gsub(" ", "", b[1, ])
+##     for(col in c("N", "S", "C")) {
+##         if (sum(b[1, ] == col) > 0) {
+##             out[i_c, col] <- b[2, b[1, ] == col]
+##         }
+##     }
+## }
+## p
+
+
+## g <- read.csv("~/Downloads/genomes.csv")
+## ## add on matches if possible!
+## g$commonS <- out[match(g[, 1], out[, "N"]), "S"]
+## g$commonC <- out[match(g[, 1], out[, "N"]), "C"]
+
+## g2 <- g[grep("Animals", g[, 2]), ]
+
+##   Eukaryota;Animals;Amphibians                                                        2
+##   Eukaryota;Animals;Birds                                                             9
+##   Eukaryota;Animals;Fishes                                                           19
+##   Eukaryota;Animals;Flatworms                                                         1
+##   Eukaryota;Animals;Insects                                                          16
+##   Eukaryota;Animals;Mammals                                                          32
+##   Eukaryota;Animals;Other Animals                                                     3
+##   Eukaryota;Animals;Reptiles                                                          2
+##   Eukaryota;Animals;Roundworms                                                        6
+
+
+
+## who <- "Eukaryota;Animals;Reptiles"
+## who <- "Eukaryota;Animals;Insects"
+## who <- "Eukaryota;Animals;Fishes"
+## a <- g[g[, 2] == who & g[, "Chromosomes"] > 0, ]
+
+## a[sort(unlist(apply(a, 2, function(x) grep("Salm", x)))), ]
+
+
+
+
+
+
+## table(as.character(g2[, 2]))
+## ## 
+## g[grep("Animals", g[, 2]), ][1:10, ]
+
+
+## system("curl -O ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR304/SRR304976/SRR304976.sra")
+
+## system("curl -O ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR126/SRR1264540/SRR1264540.sra")
+## ftp.sra.ebi.ac.uk/vol1/fastq/SRR126/000/SRR1264540/SRR1264540_1.fastq.gz
+
---
+++ obsolete/prepare_inputs_functions.R
@@ -0,0 +1,181 @@
+## note, a URL like this might work
+## https://www.ebi.ac.uk/ena/portal/api/filereport?accession=PRJNA317745&result=read_run&fields=study_accession,secondary_study_accession,sample_accession,secondary_sample_accession,experiment_accession,run_accession,submission_accession,tax_id,scientific_name,instrument_platform,instrument_model,library_name,nominal_length,library_layout,library_strategy,library_source,library_selection,read_count,base_count,center_name,first_public,last_updated,experiment_title,study_title,study_alias,experiment_alias,run_alias,fastq_bytes,fastq_md5,fastq_ftp,fastq_aspera,fastq_galaxy,submitted_bytes,submitted_md5,submitted_ftp,submitted_aspera,submitted_galaxy,submitted_format,sra_bytes,sra_md5,sra_ftp,sra_aspera,sra_galaxy,cram_index_ftp,cram_index_aspera,cram_index_galaxy,sample_alias,broker_name,sample_title,nominal_sdev,first_created&format=tsv&download=true
+
+## each entry in the list has the following elements, in order, unnamed
+## - name of the sample in the ENA file
+## - scientific name
+## - working name for the analysis (lower case, simple form)
+## - file to read in
+## - number of mapping pieces
+get_artiodactyla_info <- function() {
+    ## okapi, giraffe, cow, goat
+    ## whitetaileddeer, reddeer, buffalo
+    ## giraffe, okapi
+    info <- list(
+        c("giraffe NZOO", "PRJNA313910", "giraffe", "filereport_read_run_PRJNA313910_tsv.txt", 50),
+        c("okapi WOAK", "Okapia johnstoni", "okapi", "filereport_read_run_PRJNA313910_tsv.txt", 50),
+        c("Btau_HOL_Bulldog1", "Bos taurus", "cow", "filereport_read_run_PRJNA238491_tsv.txt", 50),
+        c("BGI-WGS_9", "Capra aegagrus", "goat", "filereport_read_run_SRP047212_tsv.txt", 50),
+        c("OVIR.TE", "Odocoileus virginianus texanus", "whitetaileddeer", "filereport_read_run_PRJNA317745_tsv.txt", 60),
+        c("Red deer from Hungary", "Cervus elaphus", "reddeer", "filereport_read_run_PRJNA324173_tsv.txt", 50),
+        c("Cape African Buffalo", "Syncerus caffer", "buffalo", "SRX2069824 (buffalo).txt", 50)
+    )
+    info
+}
+
+## sample_alias <- info[[i_species]][1]
+## sample_name <- info[[i_species]][3]
+## paper <- info[[i_species]][4]
+## n_mapping_pieces <- as.integer(info[[i_species]][5])
+
+## canids / foxes
+get_canid_info <- function() {
+## x <- read.table("PRJNA494815 (multiple canidae).txt", sep = "\t", header = TRUE)
+## x[order(x[, "sra_bytes"]), c("sample_alias", "sample_title", "fastq_bytes", "sample_accession", "scientific_name")]
+## x <- read.table("SRA307300 (dhole).txt", header = TRUE, sep = "\t")
+## y <- read.table("PRJNA312115 (also urocyon).txt", header = TRUE, sep = "\t")
+paper1 <- "PRJNA494815 (multiple canidae).txt"
+## 
+## gray fox,      GrayFox_GOGANRA_NPS_GF30           NA 18308882706;20098897733
+## SAMN09516312 Urocyon cinereoargenteus
+info <- list(
+    c("Reef", "Vulpes vulpes", "red_fox", "PRJNA378561 (red fox).txt", 50),
+    c("Lcu2_Pastora", "Lycalopex culpaeus", "andean_fox", "SRS523207 (andean fox).txt", 50),
+    c("Sister 1", "Lycaon pictus", "african_hunting_dog", "PRJNA488046 (african wild dog).txt", 50),
+    c("RUFZCHN00001", "Cuon alpinus", "dhole", "SRA307300 (dhole).txt", 50)    ,
+    c("GoldenJackal_Syria", "Canis aureus", "golden_jackal", paper1, 50),
+    c("AfricanGoldenWolf_Ethiopia", "Canis anthus", "african_golden_wolf", paper1, 50),
+    c("cac", "Canis latrans", "coyote", "SRS661477 (coyote).txt", 50),
+    c("GFO41F", "Urocyon cinereoargenteus", "grey_fox", "PRJNA312115 (also urocyon).txt", 50),
+    c("SMI15F", "Urocyon littoralis", "island_fox", "PRJNA312115 (also urocyon).txt", 50),
+    c("SAMD00009664", "Canis lupus familiaris", "dog", "PRJDB2266 (korean dog).txt", 50)
+)
+    return(info)
+}
+
+
+get_bird_info <- function() {
+## birds
+n_mapping_pieces <- 50
+paper1 <- "PRJEB10586 (finch recomb paper).txt"
+paper2 <- "PRJNA156703 (medium ground finch).txt"
+paper3 <- "PRJNA212872 (golden collared manakin).txt"
+paper4 <- "PRJNA212877 (rifleman).txt"
+paper5 <- "PRJNA212900 (kea).txt"
+paper6 <- "PRJNA212869 (american crow).txt"
+info <- list(
+    c("DBF", "Taeniopygia bichenovii", "double_barrelled_finch", paper1, 50),
+    c("26881", "Taeniopygia guttata", "zebra_finch", paper1, 50),
+    c("73958", "Poephila acuticauda hecki", "long_tailed_finch", paper1, 50),
+    c("Geospiza_fortis reads", "Geospiza fortis", "medium_ground_finch", paper2, 50),
+    c("BGI_N305", "Manacus vitellinus", "golden_collared_manakin", paper3, 50),
+    c("BGI_N310", "Acanthisitta chloris", "rifleman", paper4, 50),
+    c("BGI_N333", "Nestor notabilis", "kea", paper5, 50),
+    c("BGI_N302", "Corvus brachyrhynchos", "american_crow", paper6, 50)
+)
+    return(info)
+}
+
+
+get_salmon_info <- function() {
+## salmon
+info <- list(
+    c("OkisDH3", "Oncorhynchus kisutch", "coho", "PRJNA352719 (coho).txt", 100),
+    c("Swanson DH", "Oncorhynchus mykiss", "rainbow_trout", "PRJNA335610 (rainbow trout).txt", 300),
+    c("Chilliwack Gynogen DE9421", "Oncorhynchus tshawytscha", "chinook", "PRJNA416144 (chinook).txt", 100),
+    c("Salp-Liver_IW2", "Salvelinus alpinus", "arctic_char", "PRJNA348349 (arctic char).txt", 100),
+    c("1133201704016 Sally gDNA, muscle SAL280BP_PE_IL47-CWRU Sample", "Salmo salar", "atlantic_salmon", "PRJNA72713 (atlantic salmon).txt", 100)
+)
+## note - can get away with ~23 GB / 24 hours with 100 I think
+## ideally would be more like ~12 hours
+    return(info)
+}
+
+get_bat_info <- function() {
+## bats
+info <- list(
+    c("PRJNA167910.BU_THK_EF1", NA, "big_brown_bat", "PRJNA72449 (big brown bat).txt", 100),
+    c("Myotis brandtii WGS", NA, "brandts_bat", "PRJNA178678. (brandts).txt"),
+    c("Myotis davidii", NA, "davids_bat", "PRJNA171994 (davids).txt"),
+    c("Myotis rufoniger, Gosudonggul cave, Danyang, in South Korea", NA, "red_bat", "PRJNA381377 (red).txt")
+)
+##    c(NA, NA, "little_brown", "PRJNA277738 (little brown bat).txt"),
+    return(info)
+}
+
+
+get_bear_info <- function() {
+    paper1 <- "PRJNA169236 (3 bears).txt"
+    paper2 <- "SRP064940 (giant panda).txt"
+    info <- list(
+        c("PB7 N7773", NA, "polar_bear", paper1),
+        c("GRZ 100", NA, "grizzly_bear", paper1),
+        c("ABC2 Lucky", NA, "black_bear", paper1),
+        c("Generic sample from Ailuropoda melanoleuca", NA, "giant_panda", paper2)
+    )
+    return(info)
+}
+
+get_b <- function(paper, sample_alias) {
+    b <- read.table(file.path(ENA_DIR, paper),  header = TRUE, sep = "\t")
+    b <- b[(b[, "sample_alias"] == sample_alias), ]
+    if (sample_alias == "Geospiza_fortis reads") {
+        b <- b[
+            (b[, "nominal_length"] >= 200) &
+            (b[, "nominal_length"] <= 500) , ]
+    } else if (sample_alias == "OkisDH3") {
+        b <- b[grep("PE400", b[, "experiment_alias"]), ]
+        b[, "nominal_length"] <- 400
+    } else if (sample_alias == "Swanson DH") {
+        b <- b[grep("Swanson_450bp", b[, "experiment_alias"]), ]
+        b[, "nominal_length"] <- 450
+    } else if (sample_alias == "Chilliwack Gynogen DE9421") {
+        b <- b[grep("HI.4075", b[, "experiment_alias"]), ]
+        b[, "nominal_length"] <- 250 ## unclear
+    } else if (sample_alias == "Salp-Liver_IW2") {
+        b <- b[grep("Illumina HiSeq 2000 sequencing; WGS ", b[, "experiment_title"]), ]
+        b[, "nominal_length"] <- 250 ## unclear
+    } else if (sample_alias == "1133201704016 Sally gDNA, muscle SAL280BP_PE_IL47-CWRU Sample") {
+        b <- b[b[, "library_strategy"] == "WGS", ]
+        ## hmm, just take hiseq
+        b <- b[grep("Illumina HiSeq", b[, "experiment_title"]), ]        
+        b <- b[b[, "nominal_length"] < 1000 , ]
+    } else if (sample_alias == "PRJNA167910.BU_THK_EF1") {
+        b <- b[b[, "nominal_length"] == 180, ]
+        b <- b[b[, "sra_bytes"] > 33000000000, ]
+        ## honestly, 2 should be enough
+        b <- b[b[, "run_accession"] %in% c("SRR363679", "SRR363680"), ]
+    } else if (sample_alias == "Myotis rufoniger, Gosudonggul cave, Danyang, in South Korea") {
+        b <- b
+    } else if (sample_alias == "Reef") {
+        read_length <- b[, "base_count"] / b[, "read_count"] / 2
+        b <- b[read_length == 150, ]
+    } else if (sample_alias == "SAMD00009664") {
+        b[, "library_name"] <- "lib1"
+    } else if (sample_alias == "PB7 N7773") {
+        b <- b[b[, "library_name"] == "232C", ]
+        b <- b[b[, "run_accession"] == "SRR518683", ]
+    } else if (sample_alias == "GRZ 100") {
+        b <- b[b[, "run_accession"] == "SRR518713", ]
+    } else if (sample_alias == "Generic sample from Ailuropoda melanoleuca") {
+        ## b <- b[b[, "run_accession"] == "SRR518713", ]
+    } else if (sample_alias == "okapi WOAK") {
+        b <- b[b[, "library_name"] == "WOAK_PE", ]
+    } else if (sample_alias == "BGI-WGS_9") {
+        b <- read.table(file.path(ENA_DIR, paper),  header = TRUE, sep = "\t")
+        ## paper suggests this is OK
+        b <- b[
+        (b[, "sample_alias"] == "BGI-WGS_9") |
+        (b[, "sample_alias"] == "BGI-WGS_5")
+      , ]
+    } else if (sample_alias == "OVIR.TE") {
+        b <- b[b[, "run_accession"] %in% c("SRR4069805", "SRR4069807", "SRR4069809"), ]
+        ## filereport_read_run_PRJNA317745_tsv.txt
+    } else {
+        b <- b[is.na(b[, "nominal_length"]) | b[, "nominal_length"] < 1000 , ]
+    }
+    b$read_length <- b$base_count / b$read_count / 2
+    return(b)
+}
+
+
